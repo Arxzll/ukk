69a4cb5fd4d8d1d3e3a63bfcd3e31a7ce72d3aea
Arxzll <aririzalfirdaus06@gmail.com>
2024-03-20 09:22:13 +0700

    a

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -42,10 +42,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="/home">Beranda</a>
+                        <a class="nav-link" href="{{url('/home')}}">Beranda</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/peminjaman">Peminjaman</a>
+                        <a class="nav-link" href="{{url('/peminjaman')}}">Peminjaman</a>
                     </li>
                  
                 </ul>
